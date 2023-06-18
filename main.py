dac623fd0d3243b2f31507af748823105be53107
pavan kumar <ramu924844@gmail.com>
2023-06-18 07:24:21 +0000

    commmit

--- main.py
+++ main.py
@@ -516,6 +516,19 @@ import torch
 import speech_recognition as sr
 from moviepy.editor import VideoFileClip
 
+from pydub import AudioSegment
+from spectralcluster import SpectralClusterer
+from resemblyzer import preprocess_wav, VoiceEncoder
+from pathlib import Path
+import os
+from spleeter.separator import Separator
+
+
+
+
+
+
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -600,4 +613,107 @@ async def upload_video(request : Request, video_file: UploadFile = File(...)):
         "video_path": video_path,
         "predicted_topic": predicted_topic_label
     }
+    
+
+
+
+
+    separator = Separator('spleeter:2stems')
+    separator.separate_to_file(audio_path, 'output')
+
+
+    audio_file_path = 'output/audio/vocals.wav'
+    wav_fpath = Path(audio_file_path)
+
+    wav = preprocess_wav(wav_fpath)
+    encoder = VoiceEncoder("cpu")
+    _, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, rate=16)
+
+
+    clusterer = SpectralClusterer(
+        min_clusters=2,
+        max_clusters=100,
+        p_percentile=0.90,
+        gaussian_blur_sigma=1)
+
+    labels = clusterer.predict(cont_embeds)
+
+
+    def create_labelling(labels,wav_splits):
+        from resemblyzer import sampling_rate
+        times = [((s.start + s.stop) / 2) / sampling_rate for s in wav_splits]
+        labelling = []
+        start_time = 0
+
+        for i,time in enumerate(times):
+            if i>0 and labels[i]!=labels[i-1]:
+                temp = [str(labels[i-1]),start_time,time]
+                labelling.append(tuple(temp))
+                start_time = time
+            if i==len(times)-1:
+                temp = [str(labels[i]),start_time,time]
+                labelling.append(tuple(temp))
+
+        return labelling
+
+    labelling = create_labelling(labels,wav_splits)
+
+
+
+    list=[]
+    def split_audio(audio_file, labelling):
+        audio = AudioSegment.from_file(audio_file)
+
+        for i, (label, start_time, end_time) in enumerate(labelling):
+            start_ms = int(start_time * 1000)  # Convert start time to milliseconds
+            end_ms = int(end_time * 1000)  # Convert end time to milliseconds
+
+            segment = audio[start_ms:end_ms]  # Extract the segment
+
+            # Save the segment as a separate audio file
+            output_file = f"{label}_segment{i}.wav"
+            segment.export(output_file, format="wav")
+            list.append(output_file)
+            # print(f"Segment {i} saved as {output_file}")
+
+    # Example usage
+    audio_file = "output/audio/vocals.wav"
+    split_audio(audio_file, labelling)
+
+
+    passage = [] 
+    person="Sales-Person : "
+    for i in list:
+        recognizer = sr.Recognizer()
+
+        audio_file = "/content/"+i
+
+        with sr.AudioFile(audio_file) as source:
+            audio = recognizer.record(source)
+
+            try:
+                text=person
+                text = text+recognizer.recognize_google(audio)
+                if person=="Sales-Person : ":
+                   person="Customer : "
+                else:
+                   person="Sales-Person : "
+                passage.append(text)
+            except sr.UnknownValueError:
+                pass
+                #print("Speech recognition could not understand audio.")
+            except sr.RequestError as e:
+                pass
+                #print("Could not request results from the speech recognition service; {0}".format(e))
+        
+    
+    context = {
+        "request": request,
+        "video_path": video_path,
+        "predicted_topic": predicted_topic_label,
+        "passage":passage
+    }
+
+
+
     return templates.TemplateResponse("index.html", context)
