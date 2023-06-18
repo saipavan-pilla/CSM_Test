942d9bc5c7b04b4e36ed4a0904e020224f587e5a
pavan kumar <ramu924844@gmail.com>
2023-06-18 11:36:35 +0000

    commit

--- main.py
+++ main.py
@@ -521,10 +521,9 @@ from spectralcluster import SpectralClusterer
 from resemblyzer import preprocess_wav, VoiceEncoder
 from pathlib import Path
 import os
-from spleeter.separator import Separator
 
-
-
+import nltk
+import openai
 
 
 
@@ -608,23 +607,20 @@ async def upload_video(request : Request, video_file: UploadFile = File(...)):
     predicted_topic_label = labels[predicted_topic]
 
     # Render the result page
-    context = {
-        "request": request,
-        "video_path": video_path,
-        "predicted_topic": predicted_topic_label
-    }
     
 
 
 
 
-    separator = Separator('spleeter:2stems')
-    separator.separate_to_file(audio_path, 'output')
+    # separator = Separator('spleeter:2stems')
+    # separator.separate_to_file(audio_path, 'output')
 
 
-    audio_file_path = 'output/audio/vocals.wav'
-    wav_fpath = Path(audio_file_path)
+    # audio_file_path = 'output/audio/vocals.wav'
+    # wav_fpath = Path(audio_file_path)
 
+
+    wav_fpath = Path(audio_path) 
     wav = preprocess_wav(wav_fpath)
     encoder = VoiceEncoder("cpu")
     _, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, rate=16)
@@ -632,9 +628,7 @@ async def upload_video(request : Request, video_file: UploadFile = File(...)):
 
     clusterer = SpectralClusterer(
         min_clusters=2,
-        max_clusters=100,
-        p_percentile=0.90,
-        gaussian_blur_sigma=1)
+        max_clusters=100)
 
     labels = clusterer.predict(cont_embeds)
 
@@ -677,7 +671,7 @@ async def upload_video(request : Request, video_file: UploadFile = File(...)):
             # print(f"Segment {i} saved as {output_file}")
 
     # Example usage
-    audio_file = "output/audio/vocals.wav"
+    audio_file = "audio.wav"
     split_audio(audio_file, labelling)
 
 
@@ -686,7 +680,7 @@ async def upload_video(request : Request, video_file: UploadFile = File(...)):
     for i in list:
         recognizer = sr.Recognizer()
 
-        audio_file = "/content/"+i
+        audio_file =i
 
         with sr.AudioFile(audio_file) as source:
             audio = recognizer.record(source)
@@ -707,13 +701,65 @@ async def upload_video(request : Request, video_file: UploadFile = File(...)):
                 #print("Could not request results from the speech recognition service; {0}".format(e))
         
     
+
+    
+    recognizer = sr.Recognizer()
+    audio_file = "audio.wav"
+
+    with sr.AudioFile(audio_file) as source:
+        audio = recognizer.record(source)
+
+        try:
+            text = recognizer.recognize_google(audio)
+            print("Recognized Text:")
+            print(text)
+        except sr.UnknownValueError:
+            print("Speech recognition could not understand audio.")
+        except sr.RequestError as e:
+            print("Could not request results from the speech recognition service; {0}".format(e))
+
+
+
+    nltk.download('vader_lexicon')
+    from nltk.sentiment import SentimentIntensityAnalyzer
+    sia = SentimentIntensityAnalyzer()
+
+    x=sia.polarity_scores(text)
+    max=max(x['neg'],x['neu'],x['pos'])
+    if max==x['pos']:
+        emotion='positive'
+    elif max==x['neu']:
+        emotion='neutral'
+    else:
+        
+        emotion='negative'
+
+
+    text=text+'.Here the emotion of the customer and the sales person is '+emotion
+    text=text+'.Give us the final summary of the emotion shown by the customer to the sales person and vice versa'
+    openai.api_key = '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'
+    def chat_with_gpt3(prompt):
+        response = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt=prompt,
+            max_tokens=50,
+            temperature=0.6
+        )
+        return response.choices[0].text.strip()
+
+    print("Welcome to the ChatGPT! Type 'exit' to end the conversation.")
+    user_input = text
+    response = chat_with_gpt3(user_input)
+
+
+
     context = {
         "request": request,
         "video_path": video_path,
         "predicted_topic": predicted_topic_label,
-        "passage":passage
+        "passage":passage,
+        "response":response
     }
-
 
 
     return templates.TemplateResponse("index.html", context)
